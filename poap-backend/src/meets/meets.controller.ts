9ed793506ab61cb128a4a88934fd7821d417fb8b
Filip Smets <filipsmetsbelgium@gmail.com>
2022-02-07 13:19:12 +0100

    Enable cors and add an addFriend method

--- poap-backend/src/meets/meets.controller.ts
+++ poap-backend/src/meets/meets.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AddFriendDTO } from './AddFriendDTO';
 
 @Controller('meets')
 export class MeetsController {
-    @Get()
+    @Get('friends')
     friends(): string {
         return 'This action returns all met friends';
+    }
+
+    @Post('addFriend')
+    addFriend(@Body() addFriendDTO : AddFriendDTO): string {
+        return 'Added friend ' + addFriendDTO.data;
     }
 }
