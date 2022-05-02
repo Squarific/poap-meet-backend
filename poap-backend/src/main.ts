e3be21aa53586ea609cbc65563d39d96ba77e9b6
Avery Smets <avery@districtthree.be>
2022-05-02 15:25:18 +0200

    Rename key files

--- poap-backend/src/main.ts
+++ poap-backend/src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const httpsOptions = {
-  key: fs.readFileSync('./secrets/private-key.pem'),
-  cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  key: fs.readFileSync('./secrets/key.pem'),
+  cert: fs.readFileSync('./secrets/cert.pem'),
 };
 
 async function bootstrap() {
