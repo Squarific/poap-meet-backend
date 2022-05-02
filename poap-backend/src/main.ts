61d98e1255e028df8e5da14eae6a9a5f52f94510
Avery Smets <avery@districtthree.be>
2022-05-02 15:11:17 +0200

    Add https

--- poap-backend/src/main.ts
+++ poap-backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const httpsOptions = {
+  key: fs.readFileSync('./secrets/private-key.pem'),
+  cert: fs.readFileSync('./secrets/public-certificate.pem'),
+};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    httpsOptions,
+  });
   await app.listen(3000);
 }
 bootstrap();
