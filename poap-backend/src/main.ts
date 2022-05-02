7c30c91737c6d930cbe41791025f097d8b9adde3
Avery Smets <avery@districtthree.be>
2022-05-02 15:44:47 +0200

    Fix fs issue

--- poap-backend/src/main.ts
+++ poap-backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 const httpsOptions = {
   key: fs.readFileSync('./secrets/key.pem'),
